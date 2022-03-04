cc170c3a7a02dc74de9148276706863506986128
Kendall Perry <perry.kendall@gmail.com>
2022-03-04 15:33:24 -0500

    up through making pwd a module

--- bash.js
+++ bash.js
@@ -1,9 +1,1 @@
-process.stdout.write('prompt > ');
-process.stdin.on('data', (data)=>{
-  const pwd = "pwd"
-  const cmd = data.toString().trim();
-  process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
-})
-
-
+const pwd = require('./pwd');
